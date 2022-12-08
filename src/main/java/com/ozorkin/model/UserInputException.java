aee6f6944fa10855a1d65474db938e833c6050fb
Ozorkin <dp191192orv@gmail.com>
2022-12-08 21:43:45 +0200

    hw12

---
+++ src/main/java/com/ozorkin/model/UserInputException.java
@@ -0,0 +1,12 @@
+package com.ozorkin.model;
+
+public class UserInputException extends Throwable {
+    public UserInputException () {
+
+    }
+
+    public UserInputException (final String message) {
+        super(message);
+
+    }
+}
